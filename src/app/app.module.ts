7b6a021ffd6efdf218c76b8914ab0ece96079ee1
Mael Sigaroudi <mael.sigaroudi@gmail.com>
2019-11-02 14:53:32 +0100

    debut mise en place archi de la page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { PagePrincipalComponent } from './pagePrincipal/pagePrincipal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      PagePrincipalComponent
+   ],
+   imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatCardModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ src/app/pagePrincipal/pagePrincipal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pagePrincipal',
+  templateUrl: './pagePrincipal.component.html',
+  styleUrls: ['./pagePrincipal.component.css']
+})
+export class PagePrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
